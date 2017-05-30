3cf407f4d3abda7893d817e1479b9ff860f70ac8
Jonathan PHILIPPE <pretrine@hotmail.fr>
2017-05-30 15:54:45 +0200

    Adds colors in logger

--- src/logger.js
+++ src/logger.js
@@ -1,11 +1,21 @@
 import CONFIG from 'config'
 
 export default ((...PREFIX) => ({
-  info(...messages) {
+  _formatPrefix() {
     let name = 'Serve'
     if (Number(CONFIG.get('use.cluster')) === 1) name = `Worker:${process.pid}`
 
-    console.log(`[${name}]`, ...PREFIX.map((name) => { return `[${name}]` }), ...messages)
+    let prefixFormated = PREFIX.map((name) => { return `[${name}]` })
+    if (prefixFormated.length !== 0) {
+      prefixFormated[0] = `\x1b[33m${prefixFormated[0]}`
+      prefixFormated[prefixFormated.length - 1] = `${prefixFormated[prefixFormated.length - 1]}\x1b[0m`
+    }
+
+    return [`\x1b[35m[${name}]\x1b[0m`, ...prefixFormated]
+  },
+
+  info(...messages) {
+    console.log(...this._formatPrefix(), ...messages)
   },
 
   socket(socket, ...messages) {
